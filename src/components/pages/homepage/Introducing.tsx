b917d1adb550435cf2eaf662317b4a697a9d41be
aavaldez64 <aaronvaldez@h2d.dev>
2023-09-27 01:49:19 -0400

    add animations in first section homepage

--- src/components/pages/homepage/Introducing.tsx
+++ src/components/pages/homepage/Introducing.tsx
@@ -1,23 +1,53 @@
+"use client";
+
+import style from "@/styles/introducing.module.css";
 import Image from 'next/image';
 import { IconWrapper } from '@/components/icons/IconWrapper';
 import { Introducing as IntroducingProps } from "@/interfaces"
+import { useEffect, useRef, useState } from "react";
 
 export const Introducing = ({ introducing }: {introducing: IntroducingProps}) => {
+    const sectionRef = useRef(null)
+    const [actionStyles, setActionStyles] = useState({
+        mainContent: style.mainContentInitial,
+        phone: style.phoneInitial,
+        tablet: style.tabletInitial,
+        wrapper: style.wrapperInitial,
+    })
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            if(entries[0].isIntersecting) {
+                // Animar contador
+                setActionStyles({
+                    mainContent: style.mainContentObserved,
+                    phone: style.phoneObserved,
+                    tablet: style.tabletObserved,
+                    wrapper: style.wrapperObserved,
+                })
+                // Desconectar observer
+              observer.disconnect();  
+            } 
+        });
+        observer.observe(sectionRef.current!);
+      
+          return () => observer.disconnect();
+    }, [])
+    
     
   return (
-    <section className="bg-[#ffe] min-h-[40rem] flex flex-col lg:flex-row items-center justify-center relative">
-        <div className="hidden lg:block absolute text-[#f1f6e9] bottom-0" style={{height: "90%"}}>
+    <section id="home-start" ref={ sectionRef } className={ style.section } >
+        <div className={`${style.wrapper} ${actionStyles.wrapper}`}>
             <IconWrapper />
         </div>
-        <div className="relative lg:absolute flex overflow-x-hidden p-8 justify-center z-50">
-            <article className="flex flex-col w-[80%] md:w-[70%] lg:w-[46.5%] gap-8 items-center justify-center">
+        <div className={`${style.mainContent} ${actionStyles.mainContent}`}>
+            <article className="flex flex-col w-[80%] md:w-[70%] lg:w-[60%] gap-8 items-center justify-center">
                 <h1 className="font-[800] text-4xl text-center">{ introducing?.title || "" }</h1>
                 <p className="text-center font-medium">{ introducing?.text || "" }</p>
                 <a aria-label={ introducing?.button || "" } href="#kontakt" className="nelly-btn w-fit">{ introducing?.button || "" }</a>
             </article>
         </div>
         <div className="flex overflow-x-hidden ps-[6vw] items-center justify-center md:justify-between relative w-full">
-            <article className="w-[16rem]">
+            <article className={`${style.phone} ${actionStyles.phone}`}>
                 <Image
                     src="/assets/hero-iphone.jpg"
                     alt="Nelly iPhone"
@@ -26,7 +56,7 @@ export const Introducing = ({ introducing }: {introducing: IntroducingProps}) =>
                     className=" w-full h-auto"
                 />
             </article>
-            <article className="hidden md:block w-[45rem] relative left-[30vw]">
+            <article className={`${style.tablet} ${actionStyles.tablet}`}>
                 <Image
                     src="/assets/hero-ipad-p-800.jpg"
                     alt="Nelly iPad"
